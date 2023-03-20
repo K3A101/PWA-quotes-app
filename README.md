231cc302889a7b1338b7a3f7991f907c6ec86b1e
K3A101 <keisha.alexander@hva.nl>
2023-03-20 17:53:51 +0100

    main content toegevoegd

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Stress relief- quotes - Progressive Web Apps 
 Voor het vak progressive web app, ga ik  mijn  Stess relief qoutes app refatoren naar een server side applicatie. 
+
+## Applicatie installeren
+Wat heb je nodig:
+- npm
+- nodejs
+- express
+- Template engine ejs
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
-const express = require('express')
-const app = express()
-const port = 5000
-
+const express = require('express');
+const app = express();
+const port = 5000;
+const path = require("path");
 // Setup de template engine ejs
-// app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
+
+//Gebruikt de public folder
+app.use(express.static('public'))
 
 // Home pagina
 app.get('/', (req, res) => {
@@ -20,5 +23,5 @@ app.get('/about', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on  http://localhost:${port}`)
 })
