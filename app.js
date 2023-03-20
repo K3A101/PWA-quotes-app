be6f3da26bccfba22713c1c3425662aec886fa0f
K3A101 <keisha.alexander@hva.nl>
2023-03-20 20:36:36 +0100

    body parser geinstalleerd

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-
+// Geinstalleerde packages
+const bodyParser = require('body-parser');
 const express = require('express');
+
+
 const app = express();
 const port = 5000;
 const path = require("path");
@@ -7,8 +10,17 @@ const path = require("path");
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
+
+
 //Gebruikt de public folder
 app.use(express.static('public'))
+
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 
 // Home pagina
 app.get('/', (req, res) => {
@@ -21,6 +33,7 @@ app.get('/about', (req, res) => {
     res.render("about")
     // res.send('Hello World!!!')
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on  http://localhost:${port}`)
