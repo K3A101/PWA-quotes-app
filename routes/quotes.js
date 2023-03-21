a1f084a6bc1f53c675b8322f72335f27f764fdf0
K3A101 <keisha.alexander@hva.nl>
2023-03-21 15:00:24 +0100

    objenaam verander naar een betere naam

--- routes/quotes.js
+++ routes/quotes.js
@@ -31,7 +31,7 @@ router.get('/quotes', (req, res) =>{
             res.render('quotes', {
                 title: 'Quotes', // We use this for the page title, see views/partials/head.ejs
                 pageTitle: 'All the quotes',
-                postData: body
+                quoteData: body
             });
         }
     })
@@ -47,21 +47,22 @@ router.get('/about', (req, res) => {
     
 })
 
-// // Create a route for our detail page
-// router.get('/quotes/:id', function (req, res) {
-//     request(`https://opensheet.elk.sh/14joQ9h8M0ydoJJ-fNYN68ls3TWPCvk8ZvBJvUXpF1cQ/sheet1/quotes/${req.params.id}`, { json: true }, function (err, requestRes, body) {
-//         if (err) {
-//             // We got an error
-//             res.send(err);
-//         } else {
-//             // Render the page using the 'post' view and our body data
-//             res.render('post', {
-//                 title: `Post ${req.params.id}`,
-//                 postData: body
-//             });
-//         }
-//     });
-// });
+// Create a route for our detail page
+router.get('/quotes/:id', function (req, res) {
+    request(`https://opensheet.elk.sh/14joQ9h8M0ydoJJ-fNYN68ls3TWPCvk8ZvBJvUXpF1cQ/sheet1/quotes/${req.params.id}`, { json: true }, function (err, requestRes, body) {
+        if (err) {
+            // We got an error
+            res.send(err);
+        } else {
+            // Render the page using the 'post' view and our body data
+            res.render('quote', {
+                title: `quote ${req.params.id}`,
+                pageTitle: `${req.name}`,
+                quoteData: body
+            });
+        }
+    });
+});
 
 // Make sure to export the router so it becomes available on imports
 module.exports = router;
