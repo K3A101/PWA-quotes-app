893563108efd166f49a73541659178b59bbe4920
K3A101 <keisha.alexander@hva.nl>
2023-03-28 15:14:07 +0200

    offline fallbck page

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,13 @@
-const CORE_CACHE_NAME = 'cache-v2';
-const RUNTIME_CACHE_NAME = 'runtime-cache';
+const CORE_CACHE_NAME = 'cache-v1';
+const DYNAMIC_CACHE_NAME ='dynamic-cache-v1'
 const CORE_ASSETS = [
     '/',
     '/offline',
     '/css/style.css',
     'https://fonts.googleapis.com/css2?family=Gloock&family=Inter:wght@300;400;700&family=Josefin+Sans:wght@300;400;500;700&display=swap',
-    'https://fonts.gstatic.com/s/materialsymbolsoutlined/v100/kJEhBvYX7BgnkSrUwT8OhrdQw4oELdPIeeII9v6oFsI.woff2'
+    'https://fonts.gstatic.com/s/gloock/v1/Iurb6YFw84WUY4NJhRakNrc.woff2',
+    'https://fonts.gstatic.com/s/inter/v12/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7SUc.woff2'
+
     
 
 ];
@@ -30,7 +32,7 @@ self.addEventListener('activate', (event) => {
             .then(cacheNames => {
                 return Promise.all(
                     cacheNames.map(cacheName => {
-                        if (cacheName !== CORE_CACHE_NAME) {
+                        if (cacheName !== CORE_CACHE_NAME && cacheName !== DYNAMIC_CACHE_NAME) {
                             return caches.delete(cacheName);
                         }
                     })
@@ -44,8 +46,21 @@ self.addEventListener('fetch', event => {
    event.respondWith(
     caches.match(event.request).then(cacheRes =>{
         return cacheRes || fetch(event.request)
+        .then(fetchRes => {
+            return caches.open(DYNAMIC_CACHE_NAME)
+            .then(cache => {
+                cache.put(event.request.url, fetchRes.clone());
+                return fetchRes;
+
+            })
+        });
+    }).catch(() =>{
+        if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/offline')
+        }
+       
     })
-   )
+   );
 
    
     console.log('fetch event', event);
