ad5fe594017cb1bf601a259dd59456b10251807e
K3A101 <keisha.alexander@hva.nl>
2023-04-03 19:32:03 +0200

    add Minifyhtml

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 5000;
 const path = require("path");
 let options = { maxAge: '2y' }
+const minifyHTML = require('express-minify-html');
 // Setup de template engine ejs
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
@@ -14,7 +15,18 @@ app.set('view engine', 'ejs');
 
 //Gebruikt de public folder
 app.use(express.static('public', options))
-
+app.use(minifyHTML({
+    override: true,
+    exception_url: false,
+    htmlMinifier: {
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes: true,
+        removeEmptyAttributes: true,
+        minifyJS: true
+    }
+}));
 
 
 // Get our route file
