e413dd3d08eb9d2be64c4bc7bc673efbc1bd2d2c
K3A101 <keisha.alexander@hva.nl>
2023-04-03 20:53:14 +0200

    lazy-loading in img

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,13 @@
-const CORE_CACHE_NAME = 'cache-v3';
-const DYNAMIC_CACHE_NAME ='dynamic-cache-v3';
+const CORE_CACHE_NAME = 'cache-v1';
+const DYNAMIC_CACHE_NAME ='dynamic-cache-v1';
 const CORE_ASSETS = [
     '/',
     '/offline',
-    '/css/style.css',
+    '/css/style.min.css',
     'https://fonts.googleapis.com/css2?family=Gloock&family=Inter:wght@300;400;700&family=Josefin+Sans:wght@300;400;500;700&display=swap',
     'https://fonts.gstatic.com/s/gloock/v1/Iurb6YFw84WUY4NJhRakNrc.woff2',
-    'https://fonts.gstatic.com/s/inter/v12/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7SUc.woff2'
+    'https://fonts.gstatic.com/s/inter/v12/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7SUc.woff2',
+    'https://upload.wikimedia.org/wikipedia/commons/4/4e/Sir_Tim_Berners-Lee_%28cropped%29.jpg'
 
     
 
