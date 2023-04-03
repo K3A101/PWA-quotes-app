625a822162cf207fcaba7b92bfebd587c501b460
K3A101 <keisha.alexander@hva.nl>
2023-04-03 19:31:50 +0200

    change sw

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CORE_CACHE_NAME = 'cache-v2';
-const DYNAMIC_CACHE_NAME ='dynamic-cache-v2';
+const CORE_CACHE_NAME = 'cache-v3';
+const DYNAMIC_CACHE_NAME ='dynamic-cache-v3';
 const CORE_ASSETS = [
     '/',
     '/offline',
